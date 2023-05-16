414fba3e870a1d1c5a41d4a5dd3baf30fe866975
md-kamal.hossain <md-kamal.hossain@WIN10-HOSSAIN>
2023-05-16 16:59:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿bool IsGustimationCorrect = false;
+Random random = new Random();
+int randomNum = random.Next(1,101);
+Console.WriteLine(" Guess a number");
+while(!IsGustimationCorrect&&(Console.ReadKey(true).Key != ConsoleKey.X))
+{
+    Console.WriteLine("");
+    int guessedNum = Convert.ToInt32(Console.ReadLine());
+    if (guessedNum < randomNum)
+    {
+        Console.WriteLine("It is too small!");
+    }
+    else if (guessedNum > randomNum)
+    {
+        Console.WriteLine("It is too big!");
+    }
+    else 
+    {
+        Console.WriteLine("True! You won");
+    }
+}
